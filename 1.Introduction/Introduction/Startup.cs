4d6333586be594942bf9c2448e7a35b2f318d87a
RusyaOne <ra.antoshkin@gmail.com>
2021-03-17 14:47:16 +0200

    Added configuration usage from the lesson 1

--- 1.Introduction/Introduction/Startup.cs
+++ 1.Introduction/Introduction/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,6 +13,13 @@ namespace Introduction
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -32,7 +40,7 @@ namespace Introduction
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello world");
+                    await context.Response.WriteAsync(_configuration["MyConfiguration"]);
                 });
             });
         }

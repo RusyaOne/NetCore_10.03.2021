16343fcab5c3ad19e959adcb4d89fddecf98d819
RusyaOne <ra.antoshkin@gmail.com>
2021-03-22 22:38:14 +0200

    Added EF core example

---
+++ Fiction/Fiction/Controllers/CharactersController.cs
@@ -0,0 +1,25 @@
+﻿using Fiction.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiction.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private FictionDbContext _dbContext;
+
+        public CharactersController(FictionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Character"] = _dbContext.Characters.FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ Fiction/Fiction/Models/FictionDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiction.Models
+{
+    public class FictionDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+
+
+        private IConfiguration _configuration;
+
+        public FictionDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("FictionDbConnection"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Character>().HasData(
+                new Character { Id = 1, Name = "Finn Mertens", Age = 14 },
+                new Character { Id = 2, Name = "Philip Fry", Age = 25 },
+                new Character { Id = 3, Name = "Arven Undomiel", Age = 2700 });
+        }
+    }
+}

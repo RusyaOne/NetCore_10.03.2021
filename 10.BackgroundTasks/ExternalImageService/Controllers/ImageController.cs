ff452b01a73aa289c69e999bc008c81033cf7fa8
RusyaOne <ra.antoshkin@gmail.com>
2021-04-12 21:03:26 +0300

    Added external image service

---
+++ 10.BackgroundTasks/ExternalImageService/Controllers/ImageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExternalImageService.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        public IActionResult GetImage()
+        {
+            return View();
+        }
+    }
+}
